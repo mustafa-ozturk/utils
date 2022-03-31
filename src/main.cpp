ba6741e796fbf811d37fd93650d29229156b17a0
Mustafa Ozturk <mozturkpro@gmail.com>
2022-03-31 16:04:37 -0400

    add basic echo

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <string>
+
+void echo(int argc, char *argv[]);
 
 int main(int argc, char *argv[]) {
-    std::cout << "hello world\n";
+    if (std::string(argv[1]) == "echo") {
+        echo(argc, argv);
+    } else {
+        std::cout << "Available commands: \n echo <text> \n";
+    }
     return 0;
+}
+
+void echo(int argc, char *argv[]){
+    for (int i = 2; i < argc; i++) {
+        std::cout << argv[i] << " ";
+    }
+    std::cout << std::endl;
 }
